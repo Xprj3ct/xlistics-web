87868d026c8f7ff6aeb505089973006424e4ad6e
X project <107910326+Xprj3ct@users.noreply.github.com>
2022-08-12 17:22:51 +0100

    osas styling

--- src/components/WaitList.vue
+++ src/components/WaitList.vue
@@ -14,7 +14,16 @@
         >
     <v-card>
         <v-card-text color=black >
-            <h4 class="mt-4">Be the first to Know</h4   >
+          <h4 class="mt-4">Be the first to Know</h4   >
+            <v-text-field  label="Full name"></v-text-field>
+            <v-text-field label="Email"></v-text-field>
+            <v-text-field label="Phone No"></v-text-field>
+            <v-select 
+          :items="items"
+          label="Preferred Device"
+         
+        ></v-select>
+            <!-- <h4 class="mt-4">Be the first to Know</h4   >
             <v-text-field v-model="waitlist.fullname" label="Full name"></v-text-field>
             <v-text-field v-model="waitlist.email" label="Email"></v-text-field>
             <v-text-field v-model="waitlist.phone" label="Phone No"></v-text-field>
@@ -22,20 +31,27 @@
           :items="items"
           label="Preferred Device"
           v-model="waitlist.device"
-        ></v-select>
+        ></v-select> -->
             <!-- <small class="white--text">* This doesn't actually save.</small> -->
             </v-card-text>
 
             <v-card-actions>
               <v-spacer></v-spacer>
-
-              <v-btn
+          
+          <v-btn
+                text
+                color="primary"
+             
+              >
+                Submit
+              </v-btn>
+              <!-- <v-btn
                 text
                 color="primary"
                 v-on:click="handleWait"
               >
                 Submit
-              </v-btn>
+              </v-btn> -->
             </v-card-actions>
           </v-card>
         </v-dialog>
@@ -45,50 +61,60 @@
 
 <script>
 
-import { addDoc, collection, doc } from 'firebase/firestore'
-import db from '../firebase'
+// import { addDoc, collection, doc } from 'firebase/firestore'
+// import db from '../firebase'
 
- const handleWait = async (waitList) => {
-    console.log(waitList)
+//  const handleWait = async (waitList) => {
+//     console.log(waitList)
 
-        try {
+//         try {
 
-            await addDoc(collection(db, `waitList`), {
-                name: waitList.fullname,
-                email: waitList.email,
-                phone: waitList.phone,
-                os: waitList.device
+//             await addDoc(collection(db, `waitList`), {
+//                 name: waitList.fullname,
+//                 email: waitList.email,
+//                 phone: waitList.phone,
+//                 os: waitList.device
 
-            })
+//             })
 
-            setName("")
-            setEmail("")
-            setPhone("")
-            setOs("")
+//             setName("")
+//             setEmail("")
+//             setPhone("")
+//             setOs("")
 
 
-        } catch (err) {
-            console.log(err)
-        }
-    }
+//         } catch (err) {
+//             console.log(err)
+//         }
+//     }
+    
 
 export default {
     data: () => ({
         items: ['ANDROID', 'IOS'],
         dialog: false,
-        waitlist:{
-          fullname:"",
-          email:"",
-          device:"",
-          phone:""
-        }
+        
         
     }),
-    methods: {
-      handleWait(this.waitlist)
-    }
-
 }
+
+// export default {
+//     data: () => ({
+//         items: ['ANDROID', 'IOS'],
+//         dialog: false,
+//         waitlist:{
+//           fullname:"",
+//           email:"",
+//           device:"",
+//           phone:""
+//         }
+        
+//     }),
+//     methods: {
+//       handleWait(this.waitlist)
+//     }
+
+// }
 </script>
 
 <style>
