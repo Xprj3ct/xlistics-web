7d32c7a85f490d1879476bc2de47b67d5d8852eb
Joel <xpaceteam@gmail.com>
2022-08-15 11:50:20 +0100

    new

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -2,26 +2,26 @@
 <div >
     <v-img src="about.png" class="mx-5" contain max-height="100"  max-width="300"/>
     <v-parallax
-    height="1500"
+    height="2000"
     src="bgg.png"
   >
         <v-row elevation="16">
             <v-col col="6">
-                <v-img src="1.png" transition="slide-x-transition" contain  max-height="300"/>
+                <v-img src="1.png" transition="slide-x-transition" contain  max-height="400"/>
             </v-col>
              <v-col col="6">
-                <v-img  src="2.png" transition="slide-x-reverse-transition" contain  max-height="250"/>
+                <v-img  src="2.png" transition="slide-x-reverse-transition" contain  max-height="350"/>
             </v-col>
         </v-row>
         <v-row elevation="16">
              <v-col col="6">
-                <v-img transition="slide-x-transition" src="3.png" contain  max-height="300"/>
+                <v-img transition="slide-x-transition" src="3.png" contain  max-height="400"/>
             </v-col>
              <v-col col="6">
-                <v-img src="4.png" contain  max-height="250"/>
+                <v-img src="4.png" contain  max-height="350"/>
             </v-col>
              <v-col col="6">
-                <v-img src="5.png" contain  max-height="300"/>
+                <v-img src="5.png" contain  max-height="400"/>
             </v-col>
         </v-row>
     </v-parallax>
